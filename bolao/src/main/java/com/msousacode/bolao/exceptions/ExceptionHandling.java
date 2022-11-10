ea7c5fc2433a9d10bd3f119299c027a199010e18
msousacode <msousacode@gmail.com>
2022-11-10 14:36:45 -0300

    Melhoria da Exception para recurso não encontrado.

--- bolao/src/main/java/com/msousacode/bolao/exceptions/ExceptionHandling.java
+++ bolao/src/main/java/com/msousacode/bolao/exceptions/ExceptionHandling.java
@@ -1,6 +1,7 @@
 package com.msousacode.bolao.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +16,7 @@ public class ExceptionHandling {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
@@ -23,6 +24,11 @@ public class ExceptionHandling {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return errors;
+        return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotfoundException.class)
+    public ResponseEntity<Object> exception(ResourceNotfoundException ex) {
+        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
---
+++ bolao/src/main/java/com/msousacode/bolao/exceptions/ResourceNotfoundException.java
@@ -0,0 +1,8 @@
+package com.msousacode.bolao.exceptions;
+
+public class ResourceNotfoundException extends RuntimeException {
+
+    public ResourceNotfoundException(String msg) {
+        super(msg);
+    }
+}
