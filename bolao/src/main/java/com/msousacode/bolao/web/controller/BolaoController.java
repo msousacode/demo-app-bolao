4901f667b9038d8d57112e8f771d655f7a5e74a1
Michael <msousacode@gmail.com>
2022-11-23 23:17:56 -0300

    Ajustes na classe BolaoService.

--- bolao/src/main/java/com/msousacode/bolao/web/controller/BolaoController.java
+++ bolao/src/main/java/com/msousacode/bolao/web/controller/BolaoController.java
@@ -1,18 +1,7 @@
 package com.msousacode.bolao.web.controller;
 
-import com.msousacode.bolao.exception.ServiceException;
-import com.msousacode.bolao.persistence.entity.Bolao;
-import com.msousacode.bolao.persistence.entity.BolaoUsuario;
-import com.msousacode.bolao.persistence.entity.types.ServiceErrorsType;
-import com.msousacode.bolao.persistence.entity.types.UsuarioType;
-import com.msousacode.bolao.persistence.repository.BolaoRepository;
-import com.msousacode.bolao.persistence.repository.BolaoUsuarioRepository;
-import com.msousacode.bolao.persistence.repository.UsuarioRepository;
 import com.msousacode.bolao.service.BolaoService;
 import com.msousacode.bolao.web.dtos.BolaoDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.security.Principal;
 import java.util.UUID;
 
@@ -33,20 +21,14 @@ public class BolaoController {
 
     @PostMapping
     public ResponseEntity<BolaoDTO> cadastrar(@RequestBody @Valid BolaoDTO bolaoDTO, Principal principal) {
-        try {
-            var response = bolaoService.cadastrar(bolaoDTO, principal.getName());
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        }catch (Exception ex){
-            throw new ServiceException(HttpStatus.BAD_REQUEST, ServiceErrorsType.ERROR_WHEN_RECORDING.getMsg(), ex.getCause());
-        }
+        var response = bolaoService.cadastrar(bolaoDTO, principal.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("{bolaoId}/partidas")
     public ResponseEntity<BolaoDTO> buscarBolao(
             @PathVariable("bolaoId") @Valid @NotEmpty(message = "ID bolão não esta presente no path.") UUID bolaoId) {
 
-        var bolao = bolaoService.buscarBolao(bolaoId);
-
-        return ResponseEntity.status(HttpStatus.OK).body(bolao);
+        return ResponseEntity.status(HttpStatus.OK).body(bolaoService.buscarBolao(bolaoId));
     }
 }
